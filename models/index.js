b70ac98736cdecdddff6059b4960fb3c1db3e3e0
justin <justinrvillena@gmail.com>
2021-06-15 19:43:56 -0700

    added sql codes

--- models/index.js
+++ models/index.js
@@ -13,17 +13,17 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.belongsToMany(Post, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'user_id'
-});
+// User.belongsToMany(Post, {
+//   through: Vote,
+//   as: 'voted_posts',
+//   foreignKey: 'user_id'
+// });
 
-Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'post_id'
-});
+// Post.belongsToMany(User, {
+//   through: Vote,
+//   as: 'voted_posts',
+//   foreignKey: 'post_id'
+// });
 
 Vote.belongsTo(User, {
   foreignKey: 'user_id'
--- models/vote.js
+++ models/vote.js
@@ -1,11 +1,39 @@
-const router = require('express').Router();
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-const apiRoutes = require('./api');
+class Vote extends Model {}
 
-router.use('/api', apiRoutes);
+Vote.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vote'
+  }
+);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
-
-module.exports = router;
+module.exports = Vote;
