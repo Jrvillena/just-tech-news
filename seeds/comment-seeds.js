1a673d9f8355da03df40f52f9325cf6316403a0d
justin <justinrvillena@gmail.com>
2021-06-16 20:01:25 -0700

    14.3 done

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -22,7 +22,30 @@ const commentdata = [
     post_id: 18
   },
   {
-    comment_text: 'In hac habitasse platea dictumst.',
+    comment_text: 'In hac habitasse platea dictumst.',async function upvoteClickHandler(event) {
+      event.preventDefault();
+    
+      const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+      const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+          post_id: id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+    
+    document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
     user_id: 7,
     post_id: 5
   },
